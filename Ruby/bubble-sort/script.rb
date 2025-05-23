4737dc99ef945616b55847d95938767bb70d7791
Ryan Hessler <51040604+RHess19@users.noreply.github.com>
2025-05-23 15:33:11 -0600

    Revert to un-optimized version to refactor optimizations.

--- Ruby/bubble-sort/script.rb
+++ Ruby/bubble-sort/script.rb
@@ -4,7 +4,7 @@
 def bubble_sort(unsorted_array)
   for i in (0..unsorted_array.length-1) do
     for j in (0..unsorted_array.length-2) do
-      if unsorted_array[j] > unsorted_array[j+1] && j < unsorted_array.length-1-i # Exclude checking the last i elements of the array. These are already guaranteed to be sorted
+      if unsorted_array[j] > unsorted_array[j+1]
         temp = unsorted_array[j]
         unsorted_array[j] = unsorted_array[j+1]
         unsorted_array[j+1] = temp
@@ -13,18 +13,11 @@ def bubble_sort(unsorted_array)
   end
 
   return unsorted_array
-
-  # loop for unsorted_array.length-1
-    # loop for unsorted_array.length-2 (-2 to prevent indexing length+1 on the last pair)
-      # if unsorted_array[i] < unsorted_array[i+1] do nothing
-      # if unsorted_array[i] > unsorted_array[i+1] swap items
-      # if unsorted_array[i] == unsorted_array[i+1] do nothing
-  
-  # return unsorted_array
 end
 
 
 # AFTER FIRST WORKING MODEL
+# 1. After n iterations, we do not need to check the last n items of the array anymore since they are guaranteed to be sorted
 # 2. At the end of each iteration, check if the list is sorted by changing a flag when elements are swapped, and resetting it at the beginning of an iteration. If it is, change a flag to stop and return
 
 
