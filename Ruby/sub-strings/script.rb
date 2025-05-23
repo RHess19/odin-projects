e56ef740f194c8101d059ba82b6aa1219ba9d411
Ryan Hessler <51040604+RHess19@users.noreply.github.com>
2025-05-22 21:34:47 -0600

    Finish pseudocode.

---
+++ Ruby/sub-strings/script.rb
@@ -0,0 +1,11 @@
+matched_words = {}
+
+
+
+
+def substrings(input_string, valid_words)
+    # for each word in valid_words
+        # if word is in input_string
+            # if word is not a key in matched_words, create new entry in the hash and initialize with a value of 1
+            # if word is a key in matched_words, increment the matching entry's value
+end
