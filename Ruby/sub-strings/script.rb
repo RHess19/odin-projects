c29d0e282621ccbaa6f75062a85c03f58d07711b
Ryan Hessler <51040604+RHess19@users.noreply.github.com>
2025-05-22 22:34:53 -0600

    Significantly reduce length of solution using #scan string method.

--- Ruby/sub-strings/script.rb
+++ Ruby/sub-strings/script.rb
@@ -1,27 +1,23 @@
 print "String to search: "
-input_string = gets.chomp.split(" ")
+input_string = gets.chomp
 
 print "Valid words separated by spaces: "
 valid_words = gets.chomp.split(" ")
 
 
 def substrings(input_string, valid_words)
+    result = {}
+
     # for each word in valid_words, iterate over every word in the input_string
     # if all or part of input_word == the word from valid_words:
     #   create a new hash entry and increment to 1, or increment the existing value in the hash
-    valid_words.reduce(Hash.new(0)) do |result, word|
-        input_string.each do |input_word|
-            if input_word.include?(word)
-                # check how many times "word" appears - if more than 1, increment by the number of times "word" appears in the current input_word being examined
-                if input_word.scan("#{word}").length == 1
-                    result[word] += 1
-                elsif input_word.scan("#{word}").length > 1
-                    result[word] += input_word.scan("#{word}").length
-                end
-            end
+
+    valid_words.each do |word|
+        if input_string.scan("#{word}").length != 0
+            result[word] = input_string.scan("#{word}").length
         end
-        result
     end
+    result
 end
 
 
