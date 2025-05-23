65fe711ae8bc4493bae02073ee0169463df4a9b8
Ryan Hessler <51040604+RHess19@users.noreply.github.com>
2025-05-23 15:03:16 -0600

    Implement optimization 1: Exclude checking the last i elements of the array, where i is the number of iterations the array has gone through. These are already guaranteed to be sorted.

--- Ruby/bubble-sort/script.rb
+++ Ruby/bubble-sort/script.rb
@@ -4,7 +4,7 @@
 def bubble_sort(unsorted_array)
   for i in (0..unsorted_array.length-1) do
     for j in (0..unsorted_array.length-2) do
-      if unsorted_array[j] > unsorted_array[j+1]
+      if unsorted_array[j] > unsorted_array[j+1] && j < unsorted_array.length-1-i # Exclude checking the last i elements of the array. These are already guaranteed to be sorted
         temp = unsorted_array[j]
         unsorted_array[j] = unsorted_array[j+1]
         unsorted_array[j+1] = temp
@@ -25,11 +25,12 @@ end
 
 
 # AFTER FIRST WORKING MODEL
-# 1. After n iterations, we do not need to check the last n items of the array anymore since they are guaranteed to be sorted
 # 2. At the end of each iteration, check if the list is sorted by changing a flag when elements are swapped, and resetting it at the beginning of an iteration. If it is, change a flag to stop and return
 
 
-
+#nums = [1, 2, 3, 4, 5]
+#nums = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+#nums = [4, 1, 3, 9, 7]
 nums = [4, 3, 78, 2, 0, 2]
 result = bubble_sort(nums)
 p result
