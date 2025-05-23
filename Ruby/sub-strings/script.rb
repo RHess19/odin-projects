8daddf6249c02c3a38921d75f01a39edeaf893d0
Ryan Hessler <51040604+RHess19@users.noreply.github.com>
2025-05-22 22:24:56 -0600

    Finish project.

--- Ruby/sub-strings/script.rb
+++ Ruby/sub-strings/script.rb
@@ -1,11 +1,29 @@
-matched_words = {}
+print "String to search: "
+input_string = gets.chomp.split(" ")
 
-
+print "Valid words separated by spaces: "
+valid_words = gets.chomp.split(" ")
 
 
 def substrings(input_string, valid_words)
-    # for each word in valid_words
-        # if word is in input_string
-            # if word is not a key in matched_words, create new entry in the hash and initialize with a value of 1
-            # if word is a key in matched_words, increment the matching entry's value
+    # for each word in valid_words, iterate over every word in the input_string
+    # if all or part of input_word == the word from valid_words:
+    #   create a new hash entry and increment to 1, or increment the existing value in the hash
+    valid_words.reduce(Hash.new(0)) do |result, word|
+        input_string.each do |input_word|
+            if input_word.include?(word)
+                # check how many times "word" appears - if more than 1, increment by the number of times "word" appears in the current input_word being examined
+                if input_word.scan("#{word}").length == 1
+                    result[word] += 1
+                elsif input_word.scan("#{word}").length > 1
+                    result[word] += input_word.scan("#{word}").length
+                end
+            end
+        end
+        result
+    end
 end
+
+
+matched_words = substrings(input_string, valid_words)
+p matched_words
