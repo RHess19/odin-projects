74ca638718dd52bddacc8ad92f8be1f0511e3b44
Ryan Hessler <51040604+RHess19@users.noreply.github.com>
2025-05-23 14:59:33 -0600

    Finish un-optimized version of bubble sort.

--- Ruby/bubble-sort/script.rb
+++ Ruby/bubble-sort/script.rb
@@ -2,6 +2,18 @@
 # Output: A sorted array the same length as the input array
 # Sorts unsorted_array via the bubble sort method
 def bubble_sort(unsorted_array)
+  for i in (0..unsorted_array.length-1) do
+    for j in (0..unsorted_array.length-2) do
+      if unsorted_array[j] > unsorted_array[j+1]
+        temp = unsorted_array[j]
+        unsorted_array[j] = unsorted_array[j+1]
+        unsorted_array[j+1] = temp
+      end
+    end
+  end
+
+  return unsorted_array
+
   # loop for unsorted_array.length-1
     # loop for unsorted_array.length-2 (-2 to prevent indexing length+1 on the last pair)
       # if unsorted_array[i] < unsorted_array[i+1] do nothing
@@ -15,3 +27,9 @@ end
 # AFTER FIRST WORKING MODEL
 # 1. After n iterations, we do not need to check the last n items of the array anymore since they are guaranteed to be sorted
 # 2. At the end of each iteration, check if the list is sorted by changing a flag when elements are swapped, and resetting it at the beginning of an iteration. If it is, change a flag to stop and return
+
+
+
+nums = [4, 3, 78, 2, 0, 2]
+result = bubble_sort(nums)
+p result
