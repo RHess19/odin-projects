088b8658167eb48673d77f06db8089affe819107
Ryan Hessler <51040604+RHess19@users.noreply.github.com>
2025-05-30 20:45:10 -0600

    Create board.rb, main.rb, pseudocode.

---
+++ Ruby/tic-tac-toe/board.rb
@@ -0,0 +1,6 @@
+class Board
+
+    # members: board, current_player, winner
+    # methods: get_board, update_board_index, calculate_valid_moves, is_valid_move?, winner?
+
+end
---
+++ Ruby/tic-tac-toe/main.rb
@@ -0,0 +1,17 @@
+# Main game loop
+winner = ""
+
+while == ""
+
+    # display the current board state
+    # display the current player
+    # display the current move options to the player
+
+    # when option is selected, reset this round if they choose > or < than the allowed options
+    # or enter any other non-legal character
+
+    # if option is valid, process the choice and check for a winner
+    # flip the current player
+    # allow the loop to begin again
+
+end
