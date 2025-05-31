6646bf77036a7dc921bcfb617ccc3c636952a5bc
Ryan Hessler <51040604+RHess19@users.noreply.github.com>
2025-05-30 23:32:31 -0600

    Features implemented: Main game loop logic in main.rb board.rb: - Test and implement all methods except winner? - Restrict incorrect user inputs
    TODO:
    - Implement Board.winner?
    - Final error checking
    - Final readability, optimizations, and comments

--- Ruby/tic-tac-toe/board.rb
+++ Ruby/tic-tac-toe/board.rb
@@ -1,21 +1,26 @@
 class Board
     attr_accessor :current_player
-    attr_reader :board
+    attr_reader :board, :board_of_indexes
 
     def initialize
         @current_player = 'X'
         @board = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
+        @board_of_indexes = [[0,0],[0,1],[0,2],[1,0],[1,1],[1,2],[2,0],[2,1],[2,2]]
     end
 
 
     # Inputs:
-    #   move: number between 1 - calculate_valid_moves.length, which is the range of possible input values from the command line
+    #   move: number between 1-9, corresponding to a board location
     # Returns:
     #   True if move is valid, false otherwise. This return value is not intended to be used for anything - it is simply a value to return for the sake of returning something meaningful
     # Take in a move, check if it is valid, and update @board if it is. If it isn't, return false and do not update the board
     def process_move(move)
         if self.is_valid_move?(move)
-            self.update_board_index(self.calculate_valid_moves[move-1][0], self.calculate_valid_moves[move-1][1])
+            board[board_of_indexes[move-1][0]][board_of_indexes[move-1][1]] = self.current_player
+            self.update_player
+
+            self.winner?
+
             return true
         else
             return false
@@ -28,7 +33,9 @@ class Board
     # Returns:
     #   True if the current board layout indicates a win, False otherwise
     # Check if the current board layout has identical symbols in any legal 3-in-a-row layouts
+    # Check if the board is full - if it is and there is no winner, it's a draw
     def winner?
+        
     end
 
 
@@ -37,9 +44,10 @@ class Board
     # Returns:
     #   valid_moves: array of indexes in @board that are equal to '-'. Example: valid_moves = [[1,2], [2,0]]
     def calculate_valid_moves
-        valid_moves = board.each_with_index do |item_outer, index_outer|
-            item.each_with_index do |item_inner, index_inner|
-                valid_moves.push([index_outer, index_inner]) if board[index_outer][index_inner] == '-'
+        valid_moves = []
+        board.each.with_index do |item_outer, index_outer|
+            item_outer.each.with_index do |item_inner, index_inner|
+                valid_moves.push([index_outer, index_inner]) if item_inner == '-'
             end
         end
         return valid_moves
@@ -87,8 +95,25 @@ class Board
     #   input_number: the command line input from the user
     # Returns:
     #   True if valid, False otherwise
-    # Check if the input from the command line for the user's choice is within 1 - calculate_valid_moves.length sine those will be the numbered options at the command line
+    # Check if @board[@board_of_indexes[input_number-1][0], @board_of_indexes[input_number-1][1]] == '-' and input_number is indeed a number between 1-9
     def is_valid_move?(input_number)
-        (input_number >= 1) && (input_number <= self.calculate_valid_moves)
+        if (input_number >= 1) && (input_number <= 9)
+            return board[board_of_indexes[input_number-1][0]][board_of_indexes[input_number-1][1]] == '-'
+        else
+            return false
+        end
+    end
+
+
+    # Inputs:
+    #   None
+    # Returns:
+    #   None
+    def update_player
+        if self.current_player == 'X'
+            self.current_player = 'O'
+        elsif self.current_player == 'O'
+            self.current_player = 'X'
+        end
     end
 end
--- Ruby/tic-tac-toe/main.rb
+++ Ruby/tic-tac-toe/main.rb
@@ -1,6 +1,7 @@
 require_relative 'board'
 
 board = Board.new
+player_input = ""
 
 while !board.winner?
 
@@ -9,14 +10,26 @@ while !board.winner?
     # 4 5 6
     # 7 8 9
 
+    puts "Enter a key corresponding to the board location."
+    puts "1 2 3\n4 5 6\n7 8 9\n\n"
+
     # display the current board state
+    puts "CURRENT BOARD"
+    board.display_board
+    puts "\n\n"
+
     # display the current player
-    # display the current move options to the player
+    puts "Player #{board.current_player}'s turn.\n"
 
     # pass the current move to the board.process_move()
+    print "Move: "
+    player_input = gets.chomp.to_i
+    if !(board.process_move(player_input))
+        puts "Please enter a valid move."
+    end
 
-    # if option is valid, process the choice and check for a winner
-    # flip the current player
+    puts "----------------------------\n"
+
     # allow the loop to begin again
 
 end
