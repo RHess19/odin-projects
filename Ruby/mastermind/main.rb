7c8f0f660bea1b05a743542365bbb832d195ef87
Ryan Hessler <51040604+RHess19@users.noreply.github.com>
2025-06-04 21:20:27 -0600

    Create pseudocode in main.rb, board.rb, player.rb.

---
+++ Ruby/mastermind/main.rb
@@ -0,0 +1,16 @@
+# USE COLORIZE. REVIEW PROJECT MANAGEMENT NOTES.
+
+# white: correct, but wrong hole
+# red: correct, and right hole
+
+# create computer and player objects
+# create a board object
+
+
+
+# main game loop for 12 rounds
+
+# if over 12 rounds, game over (lose)
+# get player input
+# check guess - if correct, game over (win)
+# display board
---
+++ Ruby/mastermind/player.rb
@@ -0,0 +1,8 @@
+# Player class
+
+# Members:
+#   @guesses - nested array of all previous guesses. Each element has two elements: the guess for that round, and the up to 4 hints returned by Board#generate_hints. attr_accessor
+
+
+# Methods:
+#   #submit_guess - take in an array. add to @guesses
