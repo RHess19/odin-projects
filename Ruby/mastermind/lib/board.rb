c7c32a9688709c976b96d53d555f060c72d81550
Ryan Hessler <51040604+RHess19@users.noreply.github.com>
2025-06-08 18:55:01 -0600

    Multiple changes:
    Rewrote instructions.
    Created Board#display_instructions to hold the instructions.
    Finish Board#generate_hints (not tested yet)

--- Ruby/mastermind/lib/board.rb
+++ Ruby/mastermind/lib/board.rb
@@ -1,14 +1,18 @@
 class Board
-  attr_accessor :answer, :hints, :valid_colors, :winner
+  attr_accessor :answer, :hints, :valid_colors, :winner, :round
 
   require_relative 'colors'
 
   def initialize(player)
+    @round = 1
     @player = player
-    @valid_colors = ['p', 'o', 'g', 'r', 'b', 'y']
-    @answer = ['r', 'r', 'r', 'r']
-    @hints = [] # holds arrays of all previous hints generated
+    @valid_colors = ['p', 'o', 'g', 'r', 'b', 'y'] # not pre-colored using colorize
+    @answer = ['r', 'g', 'r', 'r'] # not pre-colored using colorize
+    @hints = [] # holds arrays of all previous hints generated. pre-colored using colorize
     @winner = false
+    @played = false # used only to prevent the game instructions from displaying again in the case the player enters an invalid guess on the first round
+
+    self.display_instructions
   end
 
   # INPUTS:
@@ -16,13 +20,16 @@ class Board
   # RETURNS:
   #   None
   def process_guess(player_guess)
+    @played = true
     # #validate_guess on player.guesses.pop[0]. if guess is NOT valid, pop 1 from player.guesses
     if !validate_guess(player_guess)
       @player.guesses.pop
+      puts "Please enter a valid guess that is 4 characters long and includes only valid characters: #{'p'.colorize(:magenta)} #{'o'.colorize(:light_red)} #{'g'.colorize(:green)} #{'r'.colorize(:red)} #{'b'.colorize(:blue)} #{'y'.colorize(:yellow)}\n\n"
     else
       if !winner?(player_guess)
         @hints.push(generate_hints(player_guess))
         self.randomize_hints
+        @round += 1
       end
     end
   end
@@ -35,7 +42,6 @@ class Board
   def display_board
     # use colorize
   end
-
 
 
   private
@@ -83,6 +89,16 @@ class Board
     # For each item in the player's guess, if player_guess[i] is equal to @answer[i], push a red O in hints
     # If player_guess[i] is NOT equal to @answer[i] BUT player_guess[i] exists elsewhere in @answer[i], push a white O in hints
     # return hints
+    player_guess.each.with_index do |item, index|
+      if player_guess[index] == @answer[index]
+        hints.push('O'.colorize(:red))
+      elsif player_guess[index] != @answer[index] && @answer.include?(player_guess[index])
+        hints.push('O'.colorize(:white))
+      end
+    end
+
+      hints.each { |item| puts item }
+      return hints
   end
 
   # INPUTS
@@ -105,5 +121,28 @@ class Board
     end
 
     return answer
+  end
+
+  # INPUTS:
+  #   None
+  # RETURNS:
+  #   None
+  # Displays the game instructions
+  def display_instructions
+    puts "##############\n# MASTERMIND #\n##############\n\n\n"
+
+    puts "In Mastermind, the objective is to guess your opponent's secret color combination. Their secret code will be 4 colors long (order matters!) and may contains the following colors:"
+    puts "#{'Purple'.colorize(:magenta)}, #{'Orange'.colorize(:light_red)}, #{'Green'.colorize(:green)}, #{'Red'.colorize(:red)}, #{'Blue'.colorize(:blue)}, #{'Yellow'.colorize(:yellow)}\n\n"
+
+    puts "Each round, make a guess as to what the secret code is by typing four characters representing the colors (and order of colors) that you think may be the secret code. Use the first letter of the color to guess that color. Example:"
+    puts "pgrr"
+    puts "The above guess signifies that you believe the secret code may be Purple, Green, Red, Red.\n\n"
+
+    puts "After each round, you will receive hints based on your guess. The hint may contain up to 4 characters (up to one for each color in your guess)."
+    puts "The four hint characters can be either a red #{'O'.colorize(:red)} or a white #{'O'.colorize(:white)}."
+    puts "A red #{'O'.colorize(:red)} means one of your guessed colors is both contained in the answer AND is placed in the right location. A white #{'O'.colorize(:white)} means one of your guesses is contained in the answer BUT in the wrong location."
+    puts "NOTE: The order of the hints does NOT correspond to the order of the colors you guessed. So, a red #{'O'.colorize(:red)} in the third hint spot does NOT mean your third color guessed is correct.\n\n"
+
+    puts "You have 12 rounds to guess the secret code. Good luck!\n\n"
   end
 end
--- Ruby/mastermind/main.rb
+++ Ruby/mastermind/main.rb
@@ -12,31 +12,16 @@ board = Board.new(player)
 player_input = ""
 
 # main game loop for 12 rounds
-round = 1
-winner = false
-while round < 13 && board.winner != true
-  # show instructions
-  if round == 1
-    puts "##############\n# MASTERMIND #\n##############\n"
-
-    puts "Each round, guess the secret color order by entering four characters from the valid colors: #{'p'.colorize(:magenta)} #{'o'.colorize(:light_red)} #{'g'.colorize(:green)} #{'r'.colorize(:red)} #{'b'.colorize(:blue)} #{'y'.colorize(:yellow)}"
-    puts "After each guess, you will receive up to 4 hints. A white hint indicates one of your guesses is correct, but in the wrong location. A red hint indicates one of your guesses is correct AND in the right location."
-    puts "Enter guesses in the format 'cccc' where each 'c' is one of the valid colors. Hints are displayed in a random order."
-    puts "You have 12 rounds to guess the correct color combination."
-  end
-
+while board.round < 13 && board.winner != true
   puts "GAME BOARD"
   board.display_board
 
   # Get player guess
-  puts "Round #{round}"
+  puts "Round #{board.round}"
   print("Guess: ")
   player_input = gets.chomp.split("")
   player.submit_guess(player_input)
   board.process_guess(player_input)
-         
-
-  round += 1
 end
 
 # END GAME
