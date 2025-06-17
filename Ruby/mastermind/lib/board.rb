0175f5383ba6297c1a26d08dd201579fceb84224
Ryan Hessler <51040604+RHess19@users.noreply.github.com>
2025-06-16 18:26:35 -0600

    Add support for the computer guessing a player's secret code. TODO: Test win and loss accuracy.

--- Ruby/mastermind/lib/board.rb
+++ Ruby/mastermind/lib/board.rb
@@ -1,19 +1,15 @@
 class Board
-  attr_accessor :answer, :hints, :valid_colors, :winner, :round, :player
+  attr_accessor :answer, :hints, :valid_colors, :winner, :round, :player, :mode
 
   require_relative 'colors'
 
   def initialize(player)
+    @mode = ""
     @round = 1
     @player = player
     @valid_colors = ['p', 'o', 'g', 'r', 'b', 'y'] # not pre-colored using colorize
-<<<<<<< HEAD
-    @answer = ['r', 'g', 'r', 'r'] # not pre-colored using colorize
     @hints = [] # holds arrays of all previous hints generated. sub-arrays contain either "red" or "white" which is later converted to red or white 'O's in #display_board
-=======
     @answer = self.generate_answer # not pre-colored using colorize
-    @hints = [] # holds arrays of all previous hints generated. pre-colored using colorize
->>>>>>> 3524a7566224cdba43e27c7ede8acd0c6102c648
     @winner = false
     @played = false # used only to prevent the game instructions from displaying again in the case the player enters an invalid guess on the first round
 
@@ -24,16 +20,49 @@ class Board
   #   Array with length 4 containing a player's guess
   # RETURNS:
   #   None
-  def process_guess(player_guess)
-    player_guess = player_guess.map do |item|
+  def validate_secret(secret)
+    secret_code = secret.map do |item|
       item.downcase
     end
 
-    @played = true
-
-    if !validate_guess(player_guess) # player's guess either isn't exactly 4 characters long or contains invalid characters
-      puts "Please enter a valid guess that is 4 characters long and includes only valid characters: #{'p'.colorize(:magenta)} #{'o'.colorize(:light_red)} #{'g'.colorize(:green)} #{'r'.colorize(:red)} #{'b'.colorize(:blue)} #{'y'.colorize(:yellow)}\n\n"
+    if !validate_guess(secret) # Use the method for validating player inputs to determine if their secret code is valid
+      puts "Please enter a secret code that is 4 characters long and includes only valid characters: #{'p'.colorize(:magenta)} #{'o'.colorize(:light_red)} #{'g'.colorize(:green)} #{'r'.colorize(:red)} #{'b'.colorize(:blue)} #{'y'.colorize(:yellow)}\n"
+      print "Secret Code: "
+      secret = gets.chomp!.split("")
+      self.validate_secret(secret)
     else
+      self.answer = secret
+    end
+  end
+
+  # INPUTS:
+  #   Array with length 4 containing a player's guess
+  # RETURNS:
+  #   None
+  def process_guess(player_guess)
+
+    if self.mode == 'C'
+      player_guess = player_guess.map do |item|
+        item.downcase
+      end
+
+      @played = true
+
+      if !validate_guess(player_guess) # player's guess either isn't exactly 4 characters long or contains invalid characters
+          puts "Please enter a valid guess that is 4 characters long and includes only valid characters: #{'p'.colorize(:magenta)} #{'o'.colorize(:light_red)} #{'g'.colorize(:green)} #{'r'.colorize(:red)} #{'b'.colorize(:blue)} #{'y'.colorize(:yellow)}\n\n"
+      else
+        winner?(player_guess)
+        @hints.push(generate_hints(player_guess))
+        @player.submit_guess(player_guess)
+        @round += 1
+      end
+    elsif self.mode == 'P'
+      player_guess = player_guess.map do |item|
+        item.downcase
+      end
+
+      @played = true
+
       winner?(player_guess)
       @hints.push(generate_hints(player_guess))
       @player.submit_guess(player_guess)
@@ -84,6 +113,19 @@ class Board
 
       puts "\n\n"
     end
+  end
+
+  # INPUTS:
+  #   None
+  # RETURNS:
+  #   Array with length 4 containing a random hidden answer
+  def generate_answer
+    answer = []
+    4.times do
+      answer.push(@valid_colors[rand(4)])
+    end
+
+    return answer
   end
 
 
@@ -152,36 +194,39 @@ class Board
   # INPUTS:
   #   None
   # RETURNS:
-  #   Array with length 4 containing a random hidden answer
-  def generate_answer
-    answer = []
-    4.times do
-      answer.push(@valid_colors[rand(4)])
-    end
-
-    return answer
-  end
-
-  # INPUTS:
-  #   None
-  # RETURNS:
   #   None
   # Displays the game instructions
   def display_instructions
-    puts "###########################\n# MASTERMIND INSTRUCTIONS #\n###########################\n\n\n"
+    if self.mode == 'C'
+      puts "###########################\n# MASTERMIND INSTRUCTIONS #\n###########################\n\n\n"
+      
+      puts "In Mastermind, the objective is to guess your opponent's secret color combination. Their secret code will be 4 colors long (order matters!) and may contains the following colors:"
+      puts "#{'Purple'.colorize(:magenta)}, #{'Orange'.colorize(:light_red)}, #{'Green'.colorize(:green)}, #{'Red'.colorize(:red)}, #{'Blue'.colorize(:blue)}, #{'Yellow'.colorize(:yellow)}\n\n"
+      
+      puts "Each round, make a guess as to what the secret code is by typing four characters representing the colors (and order of colors) that you think may be the secret code. Use the first letter of the color to guess that color. Example:"
+      puts "pgrr"
+      puts "The above guess signifies that you believe the secret code may be Purple, Green, Red, Red.\n\n"
+      
+      puts "After each round, you will receive hints based on your guess. The hint may contain up to 4 characters (up to one for each color in your guess)."
+      puts "The four hint characters can be either a red #{'O'.colorize(:red)} or a white #{'O'.colorize(:white)}."
+      puts "A red #{'O'.colorize(:red)} means one of your guessed colors is both contained in the answer AND is placed in the right location. A white #{'O'.colorize(:white)} means one of your guesses is contained in the answer BUT in the wrong location."
+      puts "NOTE: The order of the hints does NOT correspond to the order of the colors you guessed. So, a red #{'O'.colorize(:red)} in the third hint spot does NOT mean your third color guessed is correct.\n\n"
+      
+      puts "You have 12 rounds to guess the secret code. Good luck!\n\n"
+    elsif self.mode == 'P'
+      puts "###########################\n# MASTERMIND INSTRUCTIONS #\n###########################\n\n\n"
 
-    puts "In Mastermind, the objective is to guess your opponent's secret color combination. Their secret code will be 4 colors long (order matters!) and may contains the following colors:"
-    puts "#{'Purple'.colorize(:magenta)}, #{'Orange'.colorize(:light_red)}, #{'Green'.colorize(:green)}, #{'Red'.colorize(:red)}, #{'Blue'.colorize(:blue)}, #{'Yellow'.colorize(:yellow)}\n\n"
+      puts "In Mastermind, the objective is to guess your opponent's secret color combination. Their secret code will be 4 colors long (order matters!) and may contains the following colors:"
+      puts "#{'Purple'.colorize(:magenta)}, #{'Orange'.colorize(:light_red)}, #{'Green'.colorize(:green)}, #{'Red'.colorize(:red)}, #{'Blue'.colorize(:blue)}, #{'Yellow'.colorize(:yellow)}\n\n"
 
-    puts "Each round, make a guess as to what the secret code is by typing four characters representing the colors (and order of colors) that you think may be the secret code. Use the first letter of the color to guess that color. Example:"
-    puts "pgrr"
-    puts "The above guess signifies that you believe the secret code may be Purple, Green, Red, Red.\n\n"
+      puts "Each round, the computer will make a guess as to what the secret code is. For example, the computer could guess:"
+      puts "pgrr"
+      puts "The above guess signifies that the computer believes the secret code may be Purple, Green, Red, Red.\n\n"
 
-    puts "After each round, you will receive hints based on your guess. The hint may contain up to 4 characters (up to one for each color in your guess)."
-    puts "The four hint characters can be either a red #{'O'.colorize(:red)} or a white #{'O'.colorize(:white)}."
-    puts "A red #{'O'.colorize(:red)} means one of your guessed colors is both contained in the answer AND is placed in the right location. A white #{'O'.colorize(:white)} means one of your guesses is contained in the answer BUT in the wrong location."
-    puts "NOTE: The order of the hints does NOT correspond to the order of the colors you guessed. So, a red #{'O'.colorize(:red)} in the third hint spot does NOT mean your third color guessed is correct.\n\n"
-
-    puts "You have 12 rounds to guess the secret code. Good luck!\n\n"
+      puts "After each round, the computer will receive hints based on its guess. The hint may contain up to 4 characters (up to one for each color in the guess)."
+      puts "The four hint characters can be either a red #{'O'.colorize(:red)} or a white #{'O'.colorize(:white)}."
+      puts "A red #{'O'.colorize(:red)} means one of the guessed colors is both contained in the answer AND is placed in the right location. A white #{'O'.colorize(:white)} means one of the guesses is contained in the answer BUT in the wrong location."
+      puts "NOTE: The order of the hints does NOT correspond to the order of the colors you guessed. So, a red #{'O'.colorize(:red)} in the third hint spot does NOT mean the third color guessed is correct.\n\n"
+    end
   end
 end
--- Ruby/mastermind/main.rb
+++ Ruby/mastermind/main.rb
@@ -2,43 +2,103 @@ require_relative 'lib/board'
 require_relative 'lib/player'
 require_relative 'lib/colors'
 
-# create computer and player objects
-player = Player.new
+def select_mode
+  print("Enter C to play against the computer. Enter P to choose the secret colors and have the computer try to guess the answer: ")
+  mode = gets.chomp!
 
-# create a board object
-board = Board.new(player)
-
-player_input = ""
-
-# main game loop for 12 rounds
-while board.round < 13 && board.winner != true
-  board.display_board
-
-  # Get player guess
-  puts "Round #{board.round}"
-  print("Guess: ")
-  player_input = gets.chomp.split("")
-  board.process_guess(player_input)
-end
-
-# END GAME
-
-board.display_board
-# Rounds expired
-if board.winner != true
-  puts "Out of rounds. You lose! The correct answer is "
-  board.answer.each do |letter|
-    print Colors.make_colored([letter])[0]
+  if mode != 'C' && mode != 'P'
+    select_mode()
   end
 
-  print "\n\n"
-else
-  # Winner
-  print "You win! The correct color combination is "
+  ############# PLAYER V COMPUTER ###########
+  if mode == 'C'
+    player = Player.new
+    board = Board.new(player)
+    board.mode = 'C'
+    player_input = ""
 
-  board.answer.each do |letter|
-    print Colors.make_colored([letter])[0]
+    # main game loop for 12 rounds
+    while board.round < 13 && board.winner != true
+
+      board.display_board
+
+      # Get player guess
+      puts "Round #{board.round}"
+      print("Guess: ")
+      player_input = gets.chomp.split("")
+      board.process_guess(player_input)
+    end
+
+    # END GAME
+
+    board.display_board
+    # Rounds expired
+    if board.winner != true
+      puts "Out of rounds. You lose! The correct answer is "
+      board.answer.each do |letter|
+        print Colors.make_colored([letter])[0]
+      end
+
+      print "\n\n"
+    else
+      # Winner
+      print "You win! The correct color combination is "
+
+      board.answer.each do |letter|
+        print Colors.make_colored([letter])[0]
+      end
+
+      print "\n\n"
+    end
+
+  ########## COMPUTER V PLAYER #############
+  elsif mode == 'P'
+
+    computer = Player.new
+    board = Board.new(computer)
+    board.mode = 'P'
+    computer_input = ""
+
+    # Get a secret color from the player
+      puts "Please enter a secret code that is 4 characters long and includes only valid characters: #{'p'.colorize(:magenta)} #{'o'.colorize(:light_red)} #{'g'.colorize(:green)} #{'r'.colorize(:red)} #{'b'.colorize(:blue)} #{'y'.colorize(:yellow)}\n"
+      print("Secret Code: ")
+
+      secret_code = gets.chomp!.split("")
+      board.validate_secret(secret_code)
+
+    # main game loop for 12 rounds
+    while board.round < 13 && board.winner != true
+
+      board.display_board
+
+      # Get player guess
+      puts "Round #{board.round}"
+      computer_input = board.generate_answer
+      board.process_guess(computer_input)
+    end
+
+    # END GAME
+
+    board.display_board
+    # Rounds expired
+    if board.winner != true
+      puts "Out of rounds. Computer loses, you win! The correct answer is "
+      board.answer.each do |letter|
+        print Colors.make_colored([letter])[0]
+      end
+
+      print "\n\n"
+    else
+      # Winner
+      print "Computer wins! The correct color combination is "
+
+      board.answer.each do |letter|
+        print Colors.make_colored([letter])[0]
+      end
+
+      print "\n\n"
+    end
   end
-
-  print "\n\n"
 end
+
+select_mode
